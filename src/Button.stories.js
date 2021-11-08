2286f1bd2423a791abf973a06717a85ca8d6d44b
Mai Trosterud <aeropress@Arctic-Aquarium.local>
2021-11-08 11:02:40 +0200

    setup: CSS reset, head-preview, controls-sample

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { action } from '@storybook/addon-actions';
+
 
 import { Button } from './Button';
 import { Icon } from './Icon';
